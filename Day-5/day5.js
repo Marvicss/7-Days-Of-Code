253d820be1dc61934e4c75652c01e313e7e31b48
marvicss <marconivictor15@gmail.com>
2023-03-28 03:06:01 -0300

    Day 5

---
+++ Day-5/day5.js
@@ -0,0 +1,31 @@
+
+let listaFrutas = [];
+let listaDoces = [];
+let listaLaticinios = [];
+let listaCongelados = [];
+
+let verificador = "Sim"
+while(verificador == "Sim"){
+    let inputComida = prompt("Qual comida você deseja inserir ?");
+    let inputCategoria = prompt("Em qual categoria essa comida se encaixa (Frutas, Doces, Laticinios,Congelados)");
+    verificador  = prompt("Deseja continuar ?");
+   
+    if(inputCategoria === "Frutas"){
+        listaFrutas.push(inputComida);
+    }
+    if(inputCategoria === "Doces"){
+        listaDoces.push(inputComida);
+    }
+    if(inputCategoria === "Laticinios"){
+        listaLaticinios.push(inputComida);
+    }
+    if(inputCategoria === "Congelados"){
+        listaCongelados.push(inputComida);
+    }
+
+}
+    
+
+
+console.log(`Lista de Compras: \n Frutas: ${listaFrutas} \n Doces : ${listaDoces} \n Laticinios: ${listaLaticinios} \n Congelados: ${listaCongelados}`)
+
