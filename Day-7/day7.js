cb408d277992c7e5915e0765effea46863c1b175
marvicss <marconivictor15@gmail.com>
2023-04-02 00:51:22 -0300

    Identação

--- Day-7/day7.js
+++ Day-7/day7.js
@@ -1,51 +1,48 @@
-
-function somar (x,y){
-    return x+y;
+function somar(x, y) {
+  return x + y;
 }
 
-function subtrair(x,y){
-    return x-y;
+function subtrair(x, y) {
+  return x - y;
 }
 
-function multiplicar(x,y){
-    return x*y;
+function multiplicar(x, y) {
+  return x * y;
 }
 
-function dividir(x,y){
-    return x/y;
+function dividir(x, y) {
+  return x / y;
 }
 
 let continuar = true;
 
-while(continuar){
+while (continuar) {
+  let operacao = prompt("Escolhar uma operação");
 
-let operacao = prompt("Escolhar uma operação");
+  let num1;
+  let num2;
+  if (operacao != "sair") {
+    num1 = Number(prompt("Digite um numero"));
+    num2 = Number(prompt("Digite outro numero"));
+  } else {
+    continuar = false;
+  }
 
-let num1;
-let num2;
-if(operacao != "sair"){
-num1 = Number(prompt("Digite um numero"));
-num2 = Number(prompt("Digite outro numero"));
-}else{
-    continuar = false
-}
-
-switch(operacao){
-    case 'somar':
-        alert(`O resultado é ${somar(num1,num2)}`);
-        break;
-    case 'subtrair':
-        alert(`O resultado é ${subtrair(num1,num2)}`);
-        break;
-    case 'multiplicar':
-        alert(`O resultado é ${multiplicar(num1,num2)}`);
-        break;
-    case 'dividir':
-        alert(`O resultado é ${dividir(num1,num2)}`)
-        break;
-    case 'sair':
-        alert("até a proxima");
-        break;
-
-}
+  switch (operacao) {
+    case "somar":
+      alert(`O resultado é ${somar(num1, num2)}`);
+      break;
+    case "subtrair":
+      alert(`O resultado é ${subtrair(num1, num2)}`);
+      break;
+    case "multiplicar":
+      alert(`O resultado é ${multiplicar(num1, num2)}`);
+      break;
+    case "dividir":
+      alert(`O resultado é ${dividir(num1, num2)}`);
+      break;
+    case "sair":
+      alert("até a proxima");
+      break;
+  }
 }
